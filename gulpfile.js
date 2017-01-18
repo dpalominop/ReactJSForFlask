32dc3212cb64d129d9b197088ab8a0580423ec83
Daniel Palomino <dapalominop@gmail.com>
2017-01-18 11:58:17 -0500

    Funcion gulp transform  de manera manual.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 // requirements
+
 var gulp = require('gulp');
+var gulpBrowser = require("gulp-browser");
+var reactify = require('reactify');
+var del = require('del');
+var size = require('gulp-size');
+
 
 // tasks
+
+gulp.task('transform', function () {
+  var stream = gulp.src('./app/static/scripts/jsx/*.js')
+    .pipe(gulpBrowser.browserify({transform: ['reactify']}))
+    .pipe(gulp.dest('./app/static/scripts/js/'))
+    .pipe(size());
+  return stream;
+});
+
+gulp.task('del', function () {
+  // add task
+});
+
 gulp.task('default', function() {
-  console.log("hello!");
+  gulp.start('transform');
 });
